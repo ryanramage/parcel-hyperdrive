1838b4b8196210c64a2595a1a9f12e363efe24b1
Ryan Ramage <ryan.ramage@gmail.com>
2018-09-12 11:57:55 -0600

    hyperdrive next test

--- parcel-hyperdrive/index.js
+++ parcel-hyperdrive/index.js
@@ -1,15 +1,16 @@
+const key = '940571eab75e8eee5a8429da09b7f7b7e1733d414962114a918bdd6aa36d1f6f'
+const proxy = 'ws://gateway.mauve.moe:3000'
 
-const key = 'c74f8ceb1d5e50478ca0b71b2a65f60a20e383bd220519055facb5da93cbd5c6'
 const Websocket = require('websocket-stream/stream.js')
-const hyperdrive = require('hyperdrive')
+const hyperdrive = require('@jimpick/hyperdrive-next')
 const ram = require('random-access-idb')(key)
-
-const url = `ws://50.65.72.243:3000/${key}`
 const archive = hyperdrive(ram, key)
-archive.once('ready',  () => {
-  const socket = Websocket(url)
-  socket.pipe(archive.replicate()).pipe(socket)
-  const h = archive.history({tail: true, live: true})
-  h.on('data', (d) => { console.log('data:', d)  })
-})
 
+archive.once('ready', () => {
+  const socket = Websocket(`${proxy}/${key}`)
+  socket.pipe(archive.replicate()).pipe(socket)
+  archive.readFile('/ryan.json', 'utf-8', function (err, data) {
+    if (err) throw err
+    console.log(data) // prints 'json' as a string
+  })
+})
